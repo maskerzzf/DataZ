099307139b193bf5fe578dcb411edde6e3fe5313
maskerzzf <104179127+maskerzzf@users.noreply.github.com>
2023-09-02 23:32:58 +0800

    啥也没干

--- src/main/java/org/io/common/conf/Configuration.java
+++ src/main/java/org/io/common/conf/Configuration.java
@@ -1,9 +1,23 @@
 package org.io.common.conf;
 
-public class Configuration {
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONException;
 
+public class Configuration {
     private Object root;
     private String json;
 
+    private Configuration(final String json){
+        this.root = JSON.parse(json);
+    }
+
+    public Configuration from(String json){
+        boolean isJson = JSON.isValid(json);
+        if(isJson){
+            return new Configuration(json);
+        }else{
+            throw new JSONException("配置信息错误. 因为您提供的配置信息不是合法的JSON格式");
+        }
+    }
 
 }
--- src/main/java/org/io/mysql/reader/MySqlReader.java
+++ src/main/java/org/io/mysql/reader/MySqlReader.java
@@ -1,5 +1,24 @@
 package org.io.mysql.reader;
 
+import org.io.common.conf.Configuration;
+import org.io.common.util.DbUtil;
+
 public class MySqlReader {
 
+    private Configuration configuration;
+
+    public void init(){
+
+    }
+    public void post(){
+
+    }
+
+    public Configuration getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(Configuration configuration) {
+        this.configuration = configuration;
+    }
 }
